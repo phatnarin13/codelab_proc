a987bfab88aae4c414cfd5f6e20921b68b8de8f0
phat <kk0870972149@gmail.com>
2022-10-02 18:00:29 +0700

    first

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -1,11 +1,10 @@
+#urls.py
+
 from django.urls import path
-from .views import (
-    list_view, 
-    #Work exercise ,Insert code here    
-)
+from .views import *
 
 app_name = 'main'
 urlpatterns = [
     path('', list_view, name='home-list'),
-#Work exercise ,Insert code here
+    path('join-now',join, name='hangout'),#new url route
 ]
